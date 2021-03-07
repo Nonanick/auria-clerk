f1bd9fcba71706ab2da27bb63a22c98318c0d36a
Nich Frai <Nonanick@users.noreply.github.com>
2021-03-07 10:44:57 -0300

    Fixed some aliased interfaces; Upgraded dependencies;

--- src/procedure/model/index.ts
+++ src/procedure/model/index.ts
@@ -1,8 +1,16 @@
 export { IModelProcedureContext as IProcedureContext } from './context/IModelProcedureContext';
+export { IModelProcedureContext } from './context/IModelProcedureContext';
+
 export { IModelProcedureHook as IProcedureHook } from './hook/IModelProcedureHook';
+export { IModelProcedureHook } from './hook/IModelProcedureHook';
+
 export {
   ExecuteModelProcedureFunction as ExecuteProcedureFunction,
   IModelProcedure
 } from './IModelProcedure';
+
 export { IModelProcedureRequest as IProcedureRequest } from './IModelProcedureRequest';
+export { IModelProcedureRequest } from './IModelProcedureRequest';
+
 export { IModelProcedureResponse as IProcedureResponse } from './IModelProcedureResponse';
+export { IModelProcedureResponse } from './IModelProcedureResponse';

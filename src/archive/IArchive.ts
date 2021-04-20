80c73e4a2004a626e328e4d8a60e10f4fce54058
Nich Frai <Nonanick@users.noreply.github.com>
2021-04-19 23:12:11 -0300

    refactoring for procedures

--- src/archive/IArchive.ts
+++ src/archive/IArchive.ts
@@ -1,35 +1,3 @@
-import { MaybePromise } from '../error/Maybe';
-import { IEntityProcedure } from '../procedure/entity/IEntityProcedure';
-import { IEntityProcedureRequest } from '../procedure/entity/IEntityProcedureRequest';
-import { IEntityProcedureResponse } from '../procedure/entity/IEntityProcedureResponse';
-import { IModelProcedure } from '../procedure/model/IModelProcedure';
-import { IModelProcedureRequest } from '../procedure/model/IModelProcedureRequest';
-import { IModelProcedureResponse } from '../procedure/model/IModelProcedureResponse';
-import { QueryRequest } from '../query/QueryRequest';
-import { QueryResponse } from '../query/QueryResponse';
-import { ArchiveProcedureHook } from './ArchiveProcedureHook';
-import { ArchiveProcedureProxy } from './ArchiveProcedureProxy';
-
 export interface IArchive {
-
-  addModelProcedure(...procedures: IModelProcedure[]): void;
-  addEntityProcedure(...procedures: IEntityProcedure[]): void;
-
-  addProxy(...proxy: ArchiveProcedureProxy[]): void;
-  removeProxy(...proxy: ArchiveProcedureProxy[]): void;
-
-  addHook(...hook: ArchiveProcedureHook[]): void;
-  removeHook(...hook: ArchiveProcedureHook[]): void;
-
-  resolveRequest(procedureRequest: IModelProcedureRequest, context: any): MaybePromise<IModelProcedureResponse>;
-  resolveRequest(procedureRequest: IEntityProcedureRequest, context: any): MaybePromise<IEntityProcedureResponse>;
-
-  proxyProcedureRequest(req: IModelProcedureRequest, context: any): MaybePromise<IModelProcedureRequest>;
-  proxyProcedureRequest(req: IEntityProcedureRequest, context: any): MaybePromise<IEntityProcedureRequest>;
-
-  proxyProcedureResponse(res: IModelProcedureResponse): MaybePromise<IModelProcedureResponse>;
-  proxyProcedureResponse(res: IEntityProcedureResponse): MaybePromise<IEntityProcedureResponse>;
-
-  query<T = unknown>(queryRequest: QueryRequest<T>): MaybePromise<QueryResponse<T>>;
-
+  
 }
---
+++ src_old/archive/Archive.ts
@@ -0,0 +1,196 @@
+import { MaybePromise } from '../Clerk';
+import { ProcedureProxyWildcard } from '../entity/StoredEntity';
+import { Maybe } from '../error/Maybe';
+import { IProcedureRequest, IProcedureResponse } from '../procedure';
+import { IEntityProcedure } from '../procedure/entity/IEntityProcedure';
+import { IEntityProcedureRequest, implementsEntityProcedureRequest } from '../procedure/entity/IEntityProcedureRequest';
+import { IEntityProcedureResponse, implementsEntityProcedureResponse } from '../procedure/entity/IEntityProcedureResponse';
+import { IModelProcedure } from '../procedure/model/IModelProcedure';
+import { implementsModelProcedureRequest } from '../procedure/model/IModelProcedureRequest';
+import { implementsModelProcedureResponse } from '../procedure/model/IModelProcedureResponse';
+import { QueryRequest, QueryResponse } from '../query';
+import { ArchiveProcedureHook } from './ArchiveProcedureHook';
+import { ArchiveProcedureProxy } from './ArchiveProcedureProxy';
+import { IArchive } from './IArchive';
+
+type ModelRequest = IProcedureRequest;
+type ModelResponse = IProcedureResponse;
+
+type EntityRequest = IEntityProcedureRequest;
+type EntityResponse = IEntityProcedureResponse;
+
+export abstract class Archive implements IArchive {
+
+  protected _modelProcedures: Set<IModelProcedure> = new Set();
+  protected _entityProcedures: Set<IEntityProcedure> = new Set();
+
+  addModelProcedure(...procedures: IModelProcedure[]): void {
+    for (let procedure of procedures) {
+      this._modelProcedures.add(procedure);
+    }
+  }
+
+  addEntityProcedure(...procedures: IEntityProcedure[]): void {
+    for (let procedure of procedures) {
+      this._entityProcedures.add(procedure);
+    }
+  }
+
+  async proxyProcedureRequest(req: ModelRequest, context: any): MaybePromise<ModelRequest>;
+  async proxyProcedureRequest(req: EntityRequest, context: any): MaybePromise<EntityRequest>;
+  async proxyProcedureRequest(req: ModelRequest | EntityRequest, context: any): MaybePromise<ModelRequest | EntityRequest> {
+
+    const proxies = this._proxies;
+
+    for (let proxy of proxies) {
+      if (proxy.proxies !== "request") continue;
+
+      const matchesRequestedProcedure = Array.isArray(proxy.procedure)
+        ? proxy.procedure.includes(req.procedure)
+        : (proxy.procedure === req.procedure || proxy.procedure === ProcedureProxyWildcard);
+
+      // Applies to model?
+      if (
+        implementsModelProcedureRequest(req)
+        && proxy.appliesTo === "model"
+        && matchesRequestedProcedure
+      ) {
+        let proxiedRequest = await proxy.apply(req, context);
+        if (proxiedRequest instanceof Error) {
+          return proxiedRequest;
+        }
+        req = proxiedRequest;
+        continue;
+      }
+
+      // Applies to entity?
+      if (
+        implementsEntityProcedureRequest(req)
+        && proxy.appliesTo === "entity"
+        && matchesRequestedProcedure
+      ) {
+        let proxiedRequest = await proxy.apply(req, context);
+        if (proxiedRequest instanceof Error) {
+          return proxiedRequest;
+        }
+        req = proxiedRequest;
+        continue;
+      }
+    }
+
+    return req;
+  }
+
+  async proxyProcedureResponse(res: ModelResponse): MaybePromise<ModelResponse>;
+  async proxyProcedureResponse(res: EntityResponse): MaybePromise<EntityResponse>;
+  async proxyProcedureResponse(res: EntityResponse | ModelResponse): MaybePromise<EntityResponse | ModelResponse> {
+    const proxies = this._proxies;
+
+    for (let proxy of proxies) {
+      if (proxy.proxies !== "response") continue;
+
+      const matchesRequestedProcedure = Array.isArray(proxy.procedure)
+        ? proxy.procedure.includes(res.procedure)
+        : (proxy.procedure === res.procedure || proxy.procedure === ProcedureProxyWildcard);
+
+      // Applies to model?
+      if (
+        implementsModelProcedureResponse(res)
+        && proxy.appliesTo === "model"
+        && matchesRequestedProcedure
+      ) {
+        let proxiedRequest = await proxy.apply(res);
+        if (proxiedRequest instanceof Error) {
+          return proxiedRequest;
+        }
+        res = proxiedRequest;
+        continue;
+      }
+
+      // Applies to entity?
+      if (
+        implementsEntityProcedureResponse(res)
+        && proxy.appliesTo === "entity"
+        && matchesRequestedProcedure
+      ) {
+        let proxiedRequest = await proxy.apply(res);
+        if (proxiedRequest instanceof Error) {
+          return proxiedRequest;
+        }
+        res = proxiedRequest;
+        continue;
+      }
+    }
+
+    return res;
+  }
+
+  protected _proxies: ArchiveProcedureProxy[] = [];
+
+  protected _hooks: ArchiveProcedureHook[] = [];
+
+  addProxy(...proxy: ArchiveProcedureProxy[]): void {
+    this._proxies.push(...proxy);
+  }
+
+  removeProxy(...proxy: ArchiveProcedureProxy[]): void {
+    this._proxies = this._proxies.filter(p => !proxy.includes(p));
+  }
+
+  addHook(...hook: ArchiveProcedureHook[]): void {
+    this._hooks.push(...hook);
+  }
+
+  removeHook(...hook: ArchiveProcedureHook[]): void {
+    this._hooks = this._hooks.filter(h => !hook.includes(h));
+  }
+
+  async resolveRequest(request: ModelRequest, context: any): MaybePromise<ModelResponse>;
+  async resolveRequest(request: EntityRequest, context: any): MaybePromise<EntityResponse>;
+  async resolveRequest(request: ModelRequest | EntityRequest, context: any): MaybePromise<ModelResponse | EntityResponse> {
+
+    const maybeRequest = await this.proxyProcedureRequest(request as any, context);
+    if (maybeRequest instanceof Error) {
+      return maybeRequest;
+    }
+    const procedures = implementsEntityProcedureRequest(request)
+      ? this._entityProcedures.values() : this._modelProcedures.values();
+
+
+    let maybeResponse: Maybe<ModelResponse | EntityResponse>;
+    let procedureExists = false;
+
+    for (let procedure of procedures) {
+      if (procedure.name === request.procedure) {
+        procedureExists = true;
+        maybeResponse = await procedure.execute(this, request as any, context);
+        break;
+      }
+    }
+
+    if (maybeResponse! == null && !procedureExists) {
+      return new Error(
+        "Empty response for request, make sure the requested procedure '" + request.procedure + "' exists in the archive!"
+      );
+    }
+
+    if (maybeResponse! == null && procedureExists) {
+      return new Error(
+        "Empty response for request! Procedure '" + request.procedure + "' failed to generate a response!"
+      );
+    }
+
+    if (maybeResponse! instanceof Error) {
+      return maybeResponse;
+    }
+
+    const proxiedResponse = (await this.proxyProcedureResponse(maybeResponse! as any)) as Maybe<EntityResponse | ModelResponse>;
+
+    return proxiedResponse!;
+
+  }
+
+
+  abstract query<T = unknown>(queryRequest: QueryRequest<T>): MaybePromise<QueryResponse<T>>;
+
+}
